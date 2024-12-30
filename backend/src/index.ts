81787779bc9e26b89fd3d3816ce137708db1d432
Sougata <sougatadas9874@gmail.com>
2024-12-30 21:10:25 +0530

    add backend dependencies.

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+import bodyParser from "body-parser";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import cors from "cors";
